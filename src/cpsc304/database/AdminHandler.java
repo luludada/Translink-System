f47089a92b8d02d6a63223ce6caa3d7b33cd4468
JingqiZhang1102 <irena_zhang@163.com>
2020-04-02 15:46:10 -0700

    find the passenger who took all vehicles

--- src/cpsc304/database/AdminHandler.java
+++ src/cpsc304/database/AdminHandler.java
@@ -87,4 +87,21 @@ public class AdminHandler extends DatabaseConnectionHandler{
         }
         return result.toArray(new Driver[result.size()]);
     }
+
+    public void getGoodpassenger() {
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select user_id from passenger_card1"
+                    + " where not exists (select vehicle_follow_drive1.vehicle_id from vehicle_follow_drive1,"+
+                    "passenger_take_vehicle where passenger_take_vehicle.sin=passenger_card1.sin "+
+                    "except passenger_take_vehicle.vehicle_id=vehicle_follow_drive1.vehicle_id)");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("user_id"));
+            };
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+        return result.toArray(new Driver[result.size()]);
+    }
 }
