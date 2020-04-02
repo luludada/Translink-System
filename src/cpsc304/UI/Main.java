cd98f372b30cc6e0628bced7bf53a62b4002cf11
kylerhuagn <kaien_huang16@163.com>
2020-04-02 16:38:15 -0700

    ignoring out; refactor Admin.java and Main.java

--- src/cpsc304/UI/Main.java
+++ src/cpsc304/UI/Main.java
@@ -1,11 +1,20 @@
 package cpsc304.UI;
 
 import cpsc304.controller.Admin;
+import cpsc304.database.DatabaseConnectionHandler;
 
 public class Main{
 
     public static void main(String args[]) {
         Admin admin = new Admin();
+
+        DatabaseConnectionHandler dbHandler = new DatabaseConnectionHandler();
+        boolean didConnect = dbHandler.login("ora_huangk37", "a80403090");
+        if (didConnect) {
+            admin.adminHandler = dbHandler.getAdminHandler();
+        }
+
+
         admin.start();
     }
 
--- src/cpsc304/controller/Admin.java
+++ src/cpsc304/controller/Admin.java
@@ -1,6 +1,7 @@
 package cpsc304.controller;
 
 import cpsc304.UI.AdminWindow;
+import cpsc304.database.AdminHandler;
 import cpsc304.database.DatabaseConnectionHandler;
 import cpsc304.delegates.AdminWindowDelegate;
 import cpsc304.model.entities.Driver;
@@ -9,6 +10,7 @@ import cpsc304.model.entities.Driver;
 public class Admin implements AdminWindowDelegate {
 
     private AdminWindow adminWindow = null;
+    public AdminHandler adminHandler = null;
 
 
     public void start() {
@@ -17,14 +19,7 @@ public class Admin implements AdminWindowDelegate {
     }
 
     public void getAllDrivers() {
-        Driver[] drivers = null;
-        DatabaseConnectionHandler dbHandler = new DatabaseConnectionHandler();
-        boolean didConnect = dbHandler.login("ora_huangk37", "a80403090");
-        if (didConnect) {
-            //dbHandler.databaseSetup();
-            drivers = dbHandler.getAdminHandler().getAllDrivers();
-            adminWindow.displayDriverList(drivers);
-            //dbHandler.getAdminHandler().deletePassenger(505);
-        }
+        Driver[] drivers  = adminHandler.getAllDrivers();
+        adminWindow.displayDriverList(drivers);
     }
 }
